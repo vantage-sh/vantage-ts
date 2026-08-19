9124d724f1bab75af19e0ced96a4f21edea9ef6f
Jeremy Kreutzbender <jeremyk112@gmail.com>
2026-08-18 18:36:13 -0700

    Add generating some run time constants (#71)
    * feat: Add generating some run time constants.
    
    * fix: Fix formatting.

--- index.ts
+++ index.ts
@@ -7,4 +7,19 @@ export {
   pathEncode,
   VantageAPIError,
 } from "./BaseClient";
+export { VANTAGE_PROVIDERS, type VantageProvider } from "./providers";
+export {
+  VANTAGE_CHART_TYPES,
+  VANTAGE_COST_AGGREGATIONS,
+  VANTAGE_DATE_BINS,
+  VANTAGE_DATE_INTERVALS,
+  VANTAGE_NETWORK_FLOW_DIRECTIONS,
+  VANTAGE_NETWORK_FLOW_WEIGHTS,
+  type VantageChartType,
+  type VantageCostAggregation,
+  type VantageDateBin,
+  type VantageDateInterval,
+  type VantageNetworkFlowDirection,
+  type VantageNetworkFlowWeight,
+} from "./constants";
 export * from "./clientAutogen";
